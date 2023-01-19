97ab24a35a6f218c9d1f8fa81da32f62c225862e
Isaad Khan <IKhan@lcs.on.ca>
2023-01-19 12:01:26 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Isaad Khan on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
