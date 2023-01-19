5a4dda029222a27a875f8ed648a89b7a0d08d2c9
Isaad Khan <IKhan@lcs.on.ca>
2023-01-19 12:18:55 -0500

    Added equation and slider

--- TemperatureConverter/Views/CelsiusToFahrenheit.swift
+++ TemperatureConverter/Views/CelsiusToFahrenheit.swift
@@ -8,21 +8,35 @@
 import SwiftUI
 
 struct CelsiusToFahrenheit: View {
+    
+    
+   @State var celsius: Double = 0
+    var fahrenheit: Double {
+        celsius * 1.8 + 32
+    }
+    
     var body: some View {
         VStack(spacing: 15){
             
             Text("Celsius")
                 .font(.title2)
                 .bold()
+            Slider(value:$celsius,
+                   in: -35...60,
+                   label: { Text("Celsius") },
+                   minimumValueLabel: { Text("-35") },
+                   maximumValueLabel: { Text("60") })
             
-            Text("15")
+            
+            Text("\(celsius)")
                 .font(.title2)
             
             Text("Fahrenheit")
                 .font(.title2)
                 .bold()
             
-            
+            Text("\(fahrenheit)")
+                .font(.title2)
             
             
             Spacer()
